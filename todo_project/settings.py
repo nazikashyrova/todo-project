3d547d6f66aedd8aa15ed3c404db5226c94d2dc4
Nazik Ashyrova <74379673+nazikashyrova@users.noreply.github.com>
2022-03-29 20:16:33 +0300

    Update settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -50,17 +50,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = {
     'django.middleware.security.SecurityMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'whitenoise.middleware.WhiteNoiseMiddleware'
-
 }
 
 ROOT_URLCONF = 'todo_project.urls'
