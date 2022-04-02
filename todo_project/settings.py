77ebc21f257d66f6fc0230672ec01fc806de2383
Nazik Ashyrova <74379673+nazikashyrova@users.noreply.github.com>
2022-04-02 19:26:14 +0300

    Update settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = {
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware'
+
 
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
