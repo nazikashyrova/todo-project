3578b587d4b07ae362c18e4d8a0063bfb4f4d749
Nazik Ashyrova <74379673+nazikashyrova@users.noreply.github.com>
2022-04-10 15:51:22 +0300

    Add files via upload

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -53,13 +53,12 @@ MIDDLEWARE = {
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware'
-
 }
 
 ROOT_URLCONF = 'todo_project.urls'
