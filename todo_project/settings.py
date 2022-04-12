647698d120f493346f06499793a0378abce8d7a5
Nazik Ashyrova <74379673+nazikashyrova@users.noreply.github.com>
2022-04-12 20:31:26 +0300

    Add files via upload

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -27,6 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-4bl$&pyz&7%scvqtb-)^+&n!iy9qv4ohma=(z^xwf60*)vc2y2'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -148,4 +151,3 @@ EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 django_heroku.settings(locals())
-
