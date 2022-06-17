ebc1e254a5421494e61b45790b8454fc754c71db
Nazik Ashyrova <74379673+nazikashyrova@users.noreply.github.com>
2022-06-18 02:03:41 +0300

    Update settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "DJANGO_SECRET_KEY"
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['herokuapp.com', '127.0.0.1']
 
