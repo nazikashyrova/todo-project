91cc7134fb7a4da044d86c46b77543bcc8b9cdc1
Nazik Ashyrova <74379673+nazikashyrova@users.noreply.github.com>
2022-06-18 01:06:02 +0300

    Update settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-4bl$&pyz&7%scvqtb-)^+&n!iy9qv4ohma=(z^xwf60*)vc2y2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['herokuapp.com', '127.0.0.1']
 
